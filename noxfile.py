4ff8bd8c0ce5548c4cbfb48ffaece1a8f0471d4a
John T. Wodder II <git@varonathe.org>
2018-09-17 14:43:32 +0000

    `inspect` command

--- noxfile.py
+++ noxfile.py
@@ -7,3 +7,8 @@ def test(session):
     # The `python -m` is needed here so that the current directory is added to
     # `sys.path`:
     session.run('python', '-m', 'pytest', *session.posargs, 'wheelodex', 'test')
+
+@nox.session(reuse_venv=True)
+def inspect(session):
+    session.install('-r', 'requirements.txt')
+    session.run('python', '-m', 'wheelodex.inspect', *session.posargs)
--- wheelodex/inspect/__main__.py
+++ wheelodex/inspect/__main__.py
@@ -1,13 +1,11 @@
 import json
 import sys
 from   .inspect import inspect_wheel
-from   ..util   import for_json
 
 def main():
     for wheelfile in sys.argv[1:]:
-        with open(wheelfile, 'rb') as fp:
-            about = inspect_wheel(fp)
-            print(json.dumps(about, sort_keys=True, indent=4, default=for_json))
+        about = inspect_wheel(wheelfile)
+        print(json.dumps(about, sort_keys=True, indent=4))
 
 if __name__ == '__main__':
     main()
