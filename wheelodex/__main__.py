f873d2cfd4f4b4ecbacf6fce09ca66c21e3e2aaa
John T. Wodder II <git@varonathe.org>
2018-09-18 01:30:40 +0000

    Pass config to queue_all_wheels()

--- wheelodex/__main__.py
+++ wheelodex/__main__.py
@@ -5,6 +5,7 @@ import click
 from   .                      import __version__
 from   .db                    import WheelDatabase
 from   .download.queue_wheels import queue_all_wheels
+from   .util                  import parse_memory
 
 @click.group()
 @click.option(
@@ -22,7 +23,12 @@ def main(ctx, config):
     cfg.read(config)
     ctx.obj = SimpleNamespace()
     ctx.obj.db = WheelDatabase(cfg["database"])
-    ### TODO: Read max_size and latest_only from config and store on object
+    try:
+        ctx.obj.max_size = parse_memory(cfg["collection"]["max_size"])
+    except KeyError:
+        ctx.obj.max_size = None
+    ctx.obj.latest_only \
+        = cfg.getboolean("collection", "latest_only", fallback=True)
     logging.basicConfig(
         format  = '%(asctime)s %(levelname)s %(name)s %(message)s',
         datefmt = '%Y-%m-%dT%H:%M:%S%z',
@@ -30,11 +36,15 @@ def main(ctx, config):
     )
 
 @main.command()
+### TODO: Add command-line options for setting `latest_only` and `max_size`
 @click.pass_obj
 def queue_init(obj):
-    ### TODO: Pass in latest_only and max_size from config
     with obj.db:
-        queue_all_wheels(obj.db)
+        queue_all_wheels(
+            obj.db,
+            latest_only = obj.latest_only,
+            max_size    = obj.max_size,
+        )
 
 if __name__ == '__main__':
     main()
--- wheelodex/util.py
+++ wheelodex/util.py
@@ -1,3 +1,4 @@
+import re
 from   packaging.version import parse
 
 def latest_version(versions):
@@ -8,3 +9,20 @@ def latest_version(versions):
         # "2001.01.01" becomes "2001.1.1"), leading to a 404.
     candidates = filter(lambda v: not v[0].is_prerelease, candidates)
     return max(candidates, default=[None, None])[1]
+
+def parse_memory(s):
+    """
+    >>> parse_memory('42')
+    42
+    >>> parse_memory('42k')
+    43008
+    >>> parse_memory('42 MB')
+    44040192
+    """
+    m = re.match(r'^(\d+)(?:\s*([kMGTPE])B?)?$', s)
+    if not m:
+        raise ValueError(s)
+    x = int(m.group(1))
+    if m.group(2) is not None:
+        x <<= 10 * ('kMGTPE'.index(m.group(2)) + 1)
+    return x
