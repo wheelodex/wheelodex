816dc83585e9764ed31e698cfc146b7817ec46de
John T. Wodder II <git@varonathe.org>
2018-09-22 16:31:38 +0000

    Added a "dump" command

--- wheelodex/__main__.py
+++ wheelodex/__main__.py
@@ -1,9 +1,10 @@
 from   configparser             import ConfigParser
+import json
 import logging
 from   types                    import SimpleNamespace
 import click
 from   .                        import __version__
-from   .db                      import WheelDatabase
+from   .db                      import WheelDatabase, Wheel
 from   .download.queue_wheels   import queue_all_wheels, queue_wheels_since
 from   .download.unqueue_wheels import process_queue
 from   .util                    import parse_memory
@@ -67,6 +68,36 @@ def queue_update(obj):
 def process_queue_cmd(obj):
     with obj.db:
         process_queue(obj.db)
+
+@main.command()
+@click.option('-A', '--all', 'dump_all', is_flag=True)
+@click.option('-o', '--outfile', type=click.File('w'), default='-')
+@click.option('-Q', '--queue', is_flag=True)
+@click.pass_obj
+def dump(obj, dump_all, queue, outfile):
+    with outfile, obj.db:
+        for whl in obj.db.session.query(Wheel):
+            if dump_all or whl.data is not None:
+                about = {
+                    "pypi": {
+                        "filename": whl.filename,
+                        "project": whl.project,
+                        "version": whl.version,
+                        "size": whl.size,
+                        "md5": whl.md5,
+                        "sha256": whl.sha256,
+                        "uploaded": whl.uploaded,
+                    },
+                }
+                if whl.data is not None:
+                    about["data"] = whl.data.raw_data,
+                    about["wheelodex"] = {
+                        "processed": str(whl.data.processed),
+                        "wheelodex_version": whl.data.wheelodex_version,
+                    }
+                if queue:
+                    about["queued"] = whl.queued
+                click.echo(json.dumps(about), file=outfile)
 
 if __name__ == '__main__':
     main(prog_name=__package__)
