44da488bbfbd84d534f06c19025732e00a5613cc
John T. Wodder II <git@varonathe.org>
2026-05-16 14:55:23 -0400

    wheelodex-runner.py: Combine `systemctl show` invocations into one

--- deployment/roles/wheelodex/files/wheelodex-runner.py
+++ deployment/roles/wheelodex/files/wheelodex-runner.py
@@ -27,9 +27,8 @@ def main() -> None:
         datefmt="%Y-%m-%dT%H:%M:%S%z",
         level=logging.INFO,
     )
-    service_info = {}
-    for service in SERVICES:
-        info = get_service_info(service)
+    service_info = get_service_info()
+    for service, info in service_info.items():
         if info.last_started is None:
             log.info("Service %s: never started", service)
         else:
@@ -39,47 +38,66 @@ def main() -> None:
                 info.last_started,
                 "" if info.running else "not ",
             )
-        service_info[service] = info
     if (job := get_next_job(service_info)) is not None:
         subprocess.run(["sudo", "systemctl", "start", f"{job}.service"], check=True)
 
 
-def get_service_info(service: str) -> ServiceInfo:
+def get_service_info() -> dict[str, ServiceInfo]:
+    service_info = {}
     r = subprocess.run(
         [
             "systemctl",
             "show",
             "--no-pager",
-            "--property=ActiveState,ExecMainStartTimestamp",
+            "--property=Id,ActiveState,ExecMainStartTimestamp",
             "--timestamp=unix",
-            f"{service}.service",
-        ],
+        ]
+        + [f"{s}.service" for s in SERVICES],
         check=True,
         stdout=subprocess.PIPE,
         text=True,
         encoding="utf-8",
     )
-    # Note: systemd doesn't guarantee that the properties are output in the
-    # same order as passed to --property, so using the --value option and
-    # assigning variables based on line number won't work.
-    running = False
-    last_started = None
-    for line in r.stdout.splitlines():
-        key, _, value = line.partition("=")
-        if key == "ActiveState":
-            running = value in {"active", "activating"}
-        elif key == "ExecMainStartTimestamp":
-            if value:
-                last_started = datetime.fromtimestamp(
-                    int(value.removeprefix("@")), tz=UTC
-                )
+    unseen = set(SERVICES)
+    for para in r.stdout.split("\n\n"):
+        # Note: systemd doesn't guarantee that the properties are output in the
+        # same order as passed to --property, so using the --value option and
+        # assigning variables based on line number won't work.
+        sid = None
+        running = False
+        last_started = None
+        for line in para.splitlines():
+            key, _, value = line.partition("=")
+            if key == "Id":
+                sid = value.removesuffix(".service")
+            if key == "ActiveState":
+                running = value in {"active", "activating"}
+            elif key == "ExecMainStartTimestamp":
+                if value:
+                    last_started = datetime.fromtimestamp(
+                        int(value.removeprefix("@")), tz=UTC
+                    )
+                else:
+                    last_started = None
             else:
-                last_started = None
+                raise RuntimeError(
+                    f"`systemctl show` output unexpected property {key!r}"
+                )
+        if sid is None:
+            raise RuntimeError("`systemctl show` output stanza without Id")
+        elif sid in unseen:
+            service_info[sid] = ServiceInfo(running=running, last_started=last_started)
+            unseen.discard(sid)
         else:
             raise RuntimeError(
-                f"`systemctl show {service}.service` output unexpected property {key!r}"
+                f"`systemctl show` output unexpected or duplicate service {sid!r}"
             )
-    return ServiceInfo(running=running, last_started=last_started)
+    if unseen:
+        raise RuntimeError(
+            "`systemctl show` failed to emit all requested service entries:"
+            f" {', '.join(unseen)} missing"
+        )
+    return service_info
 
 
 def get_next_job(service_info: dict[str, ServiceInfo]) -> str | None:
@@ -93,12 +111,11 @@ def get_next_job(service_info: dict[str, ServiceInfo]) -> str | None:
                 service,
             )
             return service
-    now = datetime.now(tz=UTC)
-    DAY = timedelta(days=1)
+    cutoff = datetime.now(tz=UTC) - timedelta(days=1)
     started = [
         (info.last_started, service)
         for service, info in service_info.items()
-        if info.last_started is not None and now - info.last_started > DAY
+        if info.last_started is not None and info.last_started <= cutoff
     ]
     started.sort()
     if started:
